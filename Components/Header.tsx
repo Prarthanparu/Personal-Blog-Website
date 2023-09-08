92f63671b1e6a20e3b5eefa1f0d99aa806054ab2
Prarthan Balaji <64197733+Prarthanparu@users.noreply.github.com>
2023-09-08 15:09:34 +0530

    Update Header.tsx
    style changes

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     < header className="flex justify-between p-5 mx-auto">
        <div className="flex items space-x-5 ">
            <Link href="/">
-               <img className="w-44 object-contain cursor-pointer" src="https://cheapsslsecurity.com/blog/wp-content/uploads/2015/10/Blogger-Logo.png" alt="" />
+               <img style={{width:"44px"}} src="https://cheapsslsecurity.com/blog/wp-content/uploads/2015/10/Blogger-Logo.png" alt="" />
             </Link>
             <div className="hidden md:inline-flex items-center space-x-5 ">
                 <h3>About</h3>
